077dadd1d527042f3df15caa1929419e37ff0071
Francisco Jesus Perez Pimienta <fjpimienta@gmail.com>
2025-02-08 08:56:21 -0600

    Features - Types

--- src/typeDefs/index.ts
+++ src/typeDefs/index.ts
@@ -1,11 +1,7 @@
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+import bookType from '../types/book.js';
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
+const typeDefs = `#graphql
+  ${bookType}
 
   # The "Query" type is special: it lists all of the available queries that
   # clients can execute, along with the return type for each. In this
---
+++ src/types/book.ts
@@ -0,0 +1,9 @@
+const bookType = `#graphql
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+`;
+
+export default bookType;
