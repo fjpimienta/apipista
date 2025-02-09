575129d870d6764c9c07077eb312ded34526479b
Francisco Jesus Perez Pimienta <fjpimienta@gmail.com>
2025-02-09 11:36:02 -0600

    Depuration - db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -8,19 +8,10 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGODB_URI!, {
       dbName: 'pistahielo'
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    console.log(`Connected to database: ${conn.connection.db?.databaseName}`);
-
     if (conn.connection.db) {
-      // Verificar la existencia de la colección 'students'
       const collections = await conn.connection.db.listCollections().toArray();
-      const studentsCollection = collections.find((collection: { name: string }) => collection.name === 'students');
-      if (studentsCollection) {
-        console.log("La colección 'students' existe.");
-        const count = await conn.connection.db.collection('students').countDocuments();
-        console.log(`La colección 'students' tiene ${count} registros.`);
-      } else {
-        console.log("La colección 'students' no existe.");
+      if (collections.length <= 0) {
+        console.error('No hay collections.');
       }
     } else {
       console.error('La conexión a la base de datos no está definida.');
